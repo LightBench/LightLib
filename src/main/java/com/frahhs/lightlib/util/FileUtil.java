a01db6a02026d75ea0123853c9200d74e9313933
Francesco De Caro <francesco.decaro.dev@gmail.com>
2024-07-06 13:53:07 +0200

    apache common compress avoided

--- src/main/java/com/frahhs/lightlib/util/FileUtil.java
+++ src/main/java/com/frahhs/lightlib/util/FileUtil.java
@@ -1,18 +1,22 @@
 package com.frahhs.lightlib.util;
 
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
-import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
+import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.GZIPOutputStream;
 
+/**
+ * Utility class for file operations.
+ */
 public abstract class FileUtil {
+
+    /**
+     * Checks if a file is empty.
+     *
+     * @param file The file to check.
+     * @return true if the file exists and its length is 0, false otherwise.
+     * @throws IllegalArgumentException If the file does not exist.
+     */
     public static boolean isFileEmpty(File file) {
         if (!file.exists()) {
             throw new IllegalArgumentException("Cannot check the file length. The file is not found: " + file.getAbsolutePath());
@@ -20,29 +24,50 @@ public abstract class FileUtil {
         return file.length() == 0;
     }
 
+    /**
+     * Compresses a file into a .tar.gz archive and deletes the original file.
+     *
+     * @param file The file to compress.
+     * @throws RuntimeException If an error occurs during compression or file deletion.
+     */
     public static void toTarGzipFile(File file) {
-        Path path = Paths.get(file.getPath());
-        Path output = Paths.get(file.getPath().replace(".log", "") + ".tar.gz");
+        Path inputPath = file.toPath();
+        Path output = Paths.get(inputPath.getParent().toString(), inputPath.getFileName().toString().replace(".log", "") + ".tar.gz");
 
-        try (OutputStream fOut = Files.newOutputStream(output);
-             BufferedOutputStream buffOut = new BufferedOutputStream(fOut);
-             GzipCompressorOutputStream gzOut = new GzipCompressorOutputStream(buffOut);
-             TarArchiveOutputStream tOut = new TarArchiveOutputStream(gzOut)) {
+        try {
+            // Compress the file to .tar.gz
+            compressFile(file, output.toFile());
 
-            TarArchiveEntry tarEntry = new TarArchiveEntry(
-                    path.toFile(),
-                    path.getFileName().toString()
-            );
-
-            tOut.putArchiveEntry(tarEntry);
-            Files.copy(path, tOut);
-            tOut.closeArchiveEntry();
-            tOut.finish();
+            // Delete the original file
+            if (!file.delete()) {
+                throw new RuntimeException("Failed to delete log file after compression: " + file.getPath());
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        if(!file.delete())
-            throw new RuntimeException("failed to remove log file after compression: " + file.getPath());
+    /**
+     * Compresses a file using GZIP compression.
+     *
+     * @param inputFile  The input file to compress.
+     * @param outputFile The output file where compressed data will be written.
+     * @throws IOException If an I/O error occurs during compression.
+     */
+    public static void compressFile(File inputFile, File outputFile) throws IOException {
+        try (FileInputStream fis = new FileInputStream(inputFile);
+             BufferedInputStream bis = new BufferedInputStream(fis);
+             FileOutputStream fos = new FileOutputStream(outputFile);
+             BufferedOutputStream bos = new BufferedOutputStream(fos);
+             GZIPOutputStream gzipOut = new GZIPOutputStream(bos)) {
+
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = bis.read(buffer)) != -1) {
+                gzipOut.write(buffer, 0, len);
+            }
+
+            System.out.println("Compression completed successfully: " + outputFile.getAbsolutePath());
+        }
     }
 }
